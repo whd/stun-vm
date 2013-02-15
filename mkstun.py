0b72a0aa400d05d0d1a7c2509973b716c3e4ce1c
Wesley Dawson <whd@mozilla.com>
2013-02-15 02:06:27 -0800

    Comment fix

--- mkstun.py
+++ mkstun.py
@@ -3,7 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
 # routines for automated setup and deploy of stun servers in AWS regions
 # TODO termination protection, detailed monitoring
 from __future__ import print_function
