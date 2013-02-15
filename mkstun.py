868f7e3476c728c0444b5def3a1e0202041cbfaf
Wesley Dawson <whd@mozilla.com>
2013-02-15 02:05:52 -0800

    Add license block to provisioning script

--- mkstun.py
+++ mkstun.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 # routines for automated setup and deploy of stun servers in AWS regions
 # TODO termination protection, detailed monitoring
 from __future__ import print_function
